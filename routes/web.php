fe38702a4f44e3a3d487df0e3511e2201cef38cc
bagustejo <bagustejomlg@gmail.com>
2024-09-02 10:14:56 +0700

    Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,54 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
 });
+
+Route::get('/hello', function () {
+    return 'Hello Word';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+/*Route::get('/about', function () {
+    return 'Nama: Bagus Tejo Waluyo, NIM: 2141762076';
+}); */ 
+
+Route::get('/about', function () {
+    $nama = 'Bagus Tejo';
+    $nim = '2141762076'; 
+    return "Nama: $nama, NIM: $nim";
+});
+
+//1/2
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', 
+    function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
+/*Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});*/
+
+// View Routes
+Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+//PRAKTIKUM 2 CONTROLLER
+
+
+
